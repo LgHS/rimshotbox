19c3673b925422a4ac9fbb2a7cd631d02334dd48
iooner <hello@iooner.me>
2016-05-25 11:43:40 +0200

    First commit

---
+++ firmware/firmware.ino
@@ -0,0 +1,22 @@
+#define SHOOT_1 2
+#define SHOOT_2 3
+#define SHOOT_3 4
+#define SHOOT_4 5
+#define SHOOT_5 6
+#define SHOOT_6 7
+#define SHOOT_7 8
+#define SHOOT_8 9
+#define SHOOT_9 10
+#define SHOOT_10 11
+#define STOP 12
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
